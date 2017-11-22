8358ab0ece10d554ed9e21f89125c889a382f95e
Andrei Cardozo Zanatta <andrei91.zanatta@hotmail.com>
2017-11-22 11:05:39 -0200

    segundo commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,17 @@ $(document).ready(function (){
 		$('body').toggleClass('menu-active');
 	});
 });
+var click = document.getElementById('botao');
+click.addEventListener('click', fnOK);
+
+function fnOK() {
+	alert('Teste JavaScript');
+}
+function validarNumero(){
+	var num = document.getElementById('numero').value;
+	if(isNaN(num) || num >= 1 &&  num <= 10){
+		alert('input inválido! Número não pode estar entre 1 e 10');
+	}else{
+		alert('input válido!');
+	}
+}
